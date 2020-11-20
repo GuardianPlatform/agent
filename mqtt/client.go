252226765305777c72c203b248a13d2f1bfbd473
shenqidebaozi <906604588@qq.com>
2020-11-20 11:02:29 +0800

    通过 channel 发送消息

--- mqtt/client.go
+++ mqtt/client.go
@@ -1,6 +1,8 @@
 package mqtt
 
 import (
+	"encoding/json"
+	"fmt"
 	mq "github.com/eclipse/paho.mqtt.golang"
 	"log"
 	"sync"
@@ -11,9 +13,14 @@ var MqttMessageHandler mq.MessageHandler = func(client mq.Client, message mq.Mes
 	log.Println(string(message.Payload()))
 }
 
-func Push(client mq.Client, ch chan interface{},wg *sync.WaitGroup)  {
+func Push(client mq.Client,  ch <-chan  interface{},wg *sync.WaitGroup)  {
 	defer wg.Done()
 	for i := range ch {
-		client.Publish("testtopic",0,false,i)
+		fmt.Println(i)
+		imglist, err := json.Marshal(i)
+		if err != nil {
+			panic(err.Error())
+		}
+		client.Publish("testtopic",0,false,imglist)
 	}
 }
